2727dd038d78f49b7927f0e8910a20cb81a052b2
piheqi <piheqi@jackyun.com>
2025-12-24 16:38:15 +0800

    精简

--- app/utils.ts
+++ app/utils.ts
@@ -333,15 +333,7 @@ export function supportsCustomSize(model: string): boolean {
 }
 
 export function showPlugins(provider: ServiceProvider, model: string) {
-  if (
-    provider == ServiceProvider.OpenAI ||
-    provider == ServiceProvider.Azure ||
-    provider == ServiceProvider.Moonshot ||
-    provider == ServiceProvider.ChatGLM
-  ) {
-    return true;
-  }
-  if (provider == ServiceProvider.Anthropic && !model.includes("claude-2")) {
+  if (provider == ServiceProvider.OpenAI) {
     return true;
   }
   if (provider == ServiceProvider.Google && !model.includes("vision")) {
